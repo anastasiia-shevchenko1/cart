46061de209126c12d6c02afe38fb4570753fb89c
Anastasiia_Shevchen1 <anastasiia_shevchenko1@epam.com>
2024-12-01 16:00:34 +0200

    Task-8

--- lib/cart-stack.ts
+++ lib/cart-stack.ts
@@ -1,16 +1,98 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { aws_secretsmanager as secretsmanager } from 'aws-cdk-lib';
+import { join } from 'path';
 
 export class CartStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Creating Database Credentials Secret
+    const dbCredentialsSecret = new secretsmanager.Secret(this, 'CartDBCreds', {
+      secretName: 'CartDBCredsName',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'dbmasteruser' }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'password'
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CartQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create VPC for the resources
+    const vpc = new ec2.Vpc(this, 'CartServiceVPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // RDS Database instance
+    const dbInstance = new rds.DatabaseInstance(this, 'CartRDSInstance', {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_14 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+      vpc,
+      credentials: rds.Credentials.fromSecret(dbCredentialsSecret),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      },
+      multiAz: false,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(7),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false
+    });
+
+    const lambdaFunction = new lambdaNodejs.NodejsFunction(this, 'NestJsLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: join(__dirname, '..', 'nodejs-aws-cart-api', 'dist', 'main.lambda.js'),
+      handler: 'handler',
+      bundling: {
+        externalModules: ['aws-sdk', '@nestjs/microservices', 'class-transformer', '@nestjs/websockets/socket-module', 'cache-manager', 'class-validator'],
+      },
+      environment: {
+        DB_HOST: dbInstance.dbInstanceEndpointAddress,
+        DB_PORT: dbInstance.dbInstanceEndpointPort,
+        DB_NAME: 'cartdb',
+        DB_USER: 'dbmasteruser',
+        DB_PASSWORD_SECRET: dbCredentialsSecret.secretValueFromJson('password').unsafeUnwrap(),
+      },
+      vpc,
+      allowPublicSubnet: true,
+      securityGroups: [dbInstance.connections.securityGroups[0]],
+      timeout: cdk.Duration.seconds(30)
+    });
+
+    const api = new apigateway.LambdaRestApi(this, 'CartServiceApi', {
+      handler: lambdaFunction,
+      restApiName: 'Cart Service API',
+      proxy: false,
+      deployOptions: {
+        stageName: 'dev',
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const cart = api.root.addResource('api').addResource('profile').addResource('cart');
+    cart.addMethod('GET');  // GET /api/profile/cart
+    cart.addMethod('PUT');  // PUT /api/profile/cart
+
+    const checkout = cart.addResource('checkout');
+    checkout.addMethod('POST'); // POST /api/profile/cart/checkout
   }
 }
